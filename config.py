f9850e87cdd73f205386d266c607bf1ee8d73b51
LondonIsiofia <isiofiachiji@gmail.com>
2025-11-18 14:04:41 -0500

    config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-NEWSAPI_KEY = "465c618eeb834338892424e9125967d4"
-OPENWEATHER_API_KEY = "b37355a0fd296db2e61fa11e675c2dfb"
-EMAIL_SENDER = "swagteamchiji@gmail.com"
-EMAIL_RECIEVER = "isiofiachiji@gmail.com"
-EMAIL_PASS = "mwuy nwmc wjgr liwg"
+NEWSAPI_KEY = ""
+OPENWEATHER_API_KEY = ""
+EMAIL_SENDER = ""
+EMAIL_RECIEVER = ""
+EMAIL_PASS = ""
 OUTLOOK_CLIENT_ID = os.environ.get("OUTLOOK_CLIENT_ID")
 OUTLOOK_CLIENT_SECRET = os.environ.get("OUTLOOK_CLIENT_SECRET")
 OUTLOOK_TENANT_ID = "commmon"
@@ -17,4 +17,4 @@ COINGECKO = "https://api.coingecko.com/api/v3/simple/price"
 BULL_THRESHOLD = 55.0
 ALERT_THRESHOLD = 70.0
 IPINFO_URL = "https://ipinfo.io/json"
-OPENAI_KEY = "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"
+OPENAI_KEY = ""
