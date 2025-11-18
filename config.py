6687751d0a4ea4e48b1d8400c4014004322cac79
LondonIsiofia <isiofiachiji@gmail.com>
2025-11-18 14:02:43 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+NEWSAPI_KEY = "465c618eeb834338892424e9125967d4"
+OPENWEATHER_API_KEY = "b37355a0fd296db2e61fa11e675c2dfb"
+EMAIL_SENDER = "swagteamchiji@gmail.com"
+EMAIL_RECIEVER = "isiofiachiji@gmail.com"
+EMAIL_PASS = "mwuy nwmc wjgr liwg"
+OUTLOOK_CLIENT_ID = os.environ.get("OUTLOOK_CLIENT_ID")
+OUTLOOK_CLIENT_SECRET = os.environ.get("OUTLOOK_CLIENT_SECRET")
+OUTLOOK_TENANT_ID = "commmon"
+OUTLOOK_SCOPES = ["Mail.Read"]
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+RELIABLE_SOURCES = ["Reuters", "Bloomberg", "CNBC", "CoinDesk", "CoinTelegraph", "The Wall Street Journal"]
+CRYPTO_TICKERS = ["BTC-USD", "XRP-USD", "DOGE-USD", "AVAX-USD", "VIX-USD"]
+EQ_TICKER = "SPY"
+COINGECKO = "https://api.coingecko.com/api/v3/simple/price"
+BULL_THRESHOLD = 55.0
+ALERT_THRESHOLD = 70.0
+IPINFO_URL = "https://ipinfo.io/json"
+OPENAI_KEY = "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"
